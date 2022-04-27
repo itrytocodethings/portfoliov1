05990a97b718dbb23c48b6f1fef80560d3d4cdec
itrytocodethings <imwaynebailey@gmail.com>
2022-04-27 14:29:21 -0400

    updated close nav on link click, other nav updates

--- src/js/Components/Navbar.jsx
+++ src/js/Components/Navbar.jsx
@@ -1,47 +1,53 @@
 // react utils
-import React, {useState} from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { HashLink } from "react-router-hash-link";
+
 
 // react-icons
 import { VscThreeBars } from "react-icons/vsc";
 import { VscGithubInverted } from "react-icons/vsc";
-import {BsLinkedin} from "react-icons/bs"
+import { BsLinkedin } from "react-icons/bs";
 import { FaBars } from "react-icons/fa";
-import {AiOutlineCloseCircle} from "react-icons/ai";
+import { AiOutlineCloseCircle } from "react-icons/ai";
 
 export const Navbar = () => {
   const [navOpen, setNavOpen] = useState(false);
+  const closeNav = (target) => {
+    console.log(target)
+    if (target == 'A' || target == 'BUTTON' || target == 'svg' || target == 'path') setNavOpen(false);
+  }
   return (
     <>
-      <nav class="main d-flex justify-content-between flex-wrap">
+      <nav id='main-nav' class="main d-flex justify-content-between flex-wrap">
         <div className="nav left">
           <a className="nav-link logo" aria-current="page" href="#">
-            <span className="bracket">&#60;</span><span className="initials">WB</span><span className="bracket">&#62;</span>
+            <span className="bracket">&#60;</span>
+            <span className="initials">WB</span>
+            <span className="bracket">&#62;</span>
           </a>
         </div>
         <div className="toggle-link d-none">
-          <button className="btn nav-link toggle" onClick={() => {
-            setNavOpen(true);
-          }}>
+          <button
+            className="btn nav-link toggle"
+            onClick={() => {
+              setNavOpen(true);
+            }}
+          >
             <FaBars />
           </button>
         </div>
-        <div className={`nav middle ${navOpen ? 'open' : 'close'}`}>
-          <a class="nav-link active" aria-current="page" href="#">
-            Active
-          </a>
-          <HashLink to="#two" className="nav-link">Second Div</HashLink>
-          <a class="nav-link" href="#">
-            Second Div
-          </a>
-          <a class="nav-link" href="#">
-            Link
-          </a>
-          <a class="nav-link disabled">Disabled</a>
-          <button className="btn close d-none" onClick={() => {
-            setNavOpen(false);
-          }}><AiOutlineCloseCircle/></button>
+        <div className={`nav middle ${navOpen ? "open" : "close"}`} onClick={(e) => {
+          closeNav(e.target.nodeName);
+        }}>
+          <HashLink smooth to="#about" className="nav-link">About</HashLink>
+          <HashLink smooth to="#about" className="nav-link">Projects</HashLink>
+          <HashLink smooth to="#about" className="nav-link">Contact</HashLink>
+          <button
+            className="btn close d-none"
+          >
+            <AiOutlineCloseCircle />
+          </button>
         </div>
         <div className="nav right">
           <a class="nav-link" aria-current="page" href="#">
--- src/js/Pages/Portfolio.jsx
+++ src/js/Pages/Portfolio.jsx
@@ -1,6 +1,7 @@
 // react-utils
 import React, { useState, useEffect } from "react";
 import { Navbar } from "../Components/Navbar";
+import { NavHashLink } from "react-router-hash-link";
 
 // react-icons
 import { BiArrowToTop } from "react-icons/bi";
@@ -8,24 +9,30 @@ import { BiArrowToTop } from "react-icons/bi";
 const Portfolio = () => {
   const [showPageUp, setShowPageUp] = useState(false);
   window.onscroll = () => {
-    window.scrollY > 300 ? setShowPageUp(true) : setShowPageUp(false);
-  }
+    window.scrollY > 50 ? setShowPageUp(true) : setShowPageUp(false);
+  };
   const scrollToTop = () => {
     window.scrollTo(0, 0);
-  }
+  };
   return (
     <>
       <Navbar />
       <div>
-        <button className={`btn to-top ${showPageUp ? 'visible' : 'invisible'}`} onClick={scrollToTop}>
+        <div id="about" className="container portfolio">
+          <h1>hello world :)</h1>
+        </div>
+        <div id="projects" className="container portfolio">
+          <h1>hello world :)</h1>
+        </div>
+        <div id="contact" className="container portfolio">
+          <h1>hello world :)</h1>
+        </div>
+        <button
+          className={`btn to-top ${showPageUp ? "visible" : "invisible"}`}
+          onClick={scrollToTop}
+        >
           <BiArrowToTop />
         </button>
-        <div id="one" className="container portfolio">
-          <h1>hello world :)</h1>
-        </div>
-        <div id="two" className="container portfolio">
-          <h1>hello world :)</h1>
-        </div>
       </div>
     </>
   );
